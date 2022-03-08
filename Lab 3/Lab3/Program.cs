51e043150d756bdcb3cdb3b7276805f44f9f25fe
Ivan <vanntno@gmail.com>
2022-03-08 13:35:25 +0200

    update

---
+++ Lab 3/Lab3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace BLL
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Menu.MainMenu();
+        }
+    }
+}
