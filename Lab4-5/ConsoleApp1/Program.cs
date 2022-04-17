c40f74846aa1d8de1ecae763101b0b361ec74d4c
Ivan <vanntno@gmail.com>
2022-04-17 12:27:14 +0300

    class diagrams

---
+++ Lab4-5/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Classes.BLL.Context;
+using BLL.Classes.Finder;
+using BLL.Classes.Memento;
+using DAL.Classes.Repository;
+using PL;
+using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App app = new App();
+            MainWindow m = new MainWindow(new UserContext(new EntityRepository(),new CareTaker(),new ContentFinder()));
+
+            app.Run(m);
+        }
+    }
+}
